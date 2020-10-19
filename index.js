8d242151731bef5a7a2c886757079393bed8cb0c
neil <win10@neilpang.com>
2020-10-19 22:19:01 +0800

    export ova

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const fs = require("fs");
 
 
 
-async function sleep (ms) {
+async function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms))
 }
 
@@ -19,13 +19,13 @@ async function run() {
     await exec.exec("pip3 install pytesseract");
 
 
-    let ext="https://download.virtualbox.org/virtualbox/6.1.14/Oracle_VM_VirtualBox_Extension_Pack-6.1.14.vbox-extpack";
+    let ext = "https://download.virtualbox.org/virtualbox/6.1.14/Oracle_VM_VirtualBox_Extension_Pack-6.1.14.vbox-extpack";
     //install ext for debug purpose
 
 
 
     let imgName = "FreeBSD-12.1-RELEASE-amd64";
-    let url="https://download.freebsd.org/ftp/releases/VM-IMAGES/12.1-RELEASE/amd64/Latest/"+imgName+".vhd.xz";
+    let url = "https://download.freebsd.org/ftp/releases/VM-IMAGES/12.1-RELEASE/amd64/Latest/" + imgName + ".vhd.xz";
     core.info("Downloading image: " + url);
     let img = await tc.downloadTool(url);
     core.info("Downloaded file: " + img);
@@ -36,26 +36,26 @@ async function run() {
     let vmName = "freebsd";
     core.info("Create VM");
     await exec.exec("sudo vboxmanage  createvm  --name " + vmName + " --ostype FreeBSD_64  --default   --basefolder freebsd --register");
-    
-    await exec.exec("sudo vboxmanage  storagectl " + vmName + "  --name SATA --add sata  --controller IntelAHCI ");
-    
-    await exec.exec("sudo vboxmanage  storageattach "+ vmName + "    --storagectl SATA --port 0  --device 0  --type hdd --medium " + vhd);
-    
-    await exec.exec("sudo vboxmanage modifyvm "+ vmName + " --vrde on  --vrdeport 33389");
 
-    await exec.exec("sudo vboxmanage modifyvm "+ vmName + "  --natpf1 'guestssh,tcp,,2222,,22'");
+    await exec.exec("sudo vboxmanage  storagectl " + vmName + "  --name SATA --add sata  --controller IntelAHCI ");
+
+    await exec.exec("sudo vboxmanage  storageattach " + vmName + "    --storagectl SATA --port 0  --device 0  --type hdd --medium " + vhd);
+
+    await exec.exec("sudo vboxmanage modifyvm " + vmName + " --vrde on  --vrdeport 33389");
+
+    await exec.exec("sudo vboxmanage modifyvm " + vmName + "  --natpf1 'guestssh,tcp,,2222,,22'");
 
     await exec.exec("sudo  vboxmanage modifyhd " + vhd + " --resize  100000");
-    
+
     await exec.exec("sudo vboxmanage startvm " + vmName + " --type headless");
 
 
     core.info("sleep 300 seconds for first boot");
-    let loginTag="FreeBSD/amd64 (freebsd) (ttyv0)"
+    let loginTag = "FreeBSD/amd64 (freebsd) (ttyv0)"
     let slept = 0;
-    while(true) {
-      slept +=20;
-      if(slept >= 300) {
+    while (true) {
+      slept += 20;
+      if (slept >= 300) {
         throw new Error("Timeout can not boot");
       }
       await sleep(20000);
@@ -65,13 +65,17 @@ async function run() {
 
       await exec.exec("sudo chmod 666 screen.png");
 
-      let output="";
-      await exec.exec("pytesseract  screen.png", [] , {listeners:{stdout:(s)=>{
-        output += s;
-      }}});
-    
-      
-      if(output.includes(loginTag)) {
+      let output = "";
+      await exec.exec("pytesseract  screen.png", [], {
+        listeners: {
+          stdout: (s) => {
+            output += s;
+          }
+        }
+      });
+
+
+      if (output.includes(loginTag)) {
         core.info("Login ready, sleep last 10 seconds");
         await sleep(5000);
         break;
@@ -85,46 +89,57 @@ async function run() {
 
     let home = process.env["HOME"]
     fs.appendFileSync(home + "/.ssh/config", "StrictHostKeyChecking=accept-new\n")
-    
 
-    let init = __dirname + "/init.txt"; 
+
+    let init = __dirname + "/init.txt";
     core.info(init);
     await exec.exec("sudo vboxmanage controlvm " + vmName + " keyboardputfile ", init);
 
-    await exec.exec("ssh -p 2222 root@localhost", [], {input: 'ssh-keygen -t rsa -f ~/.ssh/id_rsa -q -N ""'});
-    await exec.exec("ssh -p 2222 root@localhost", [], {input: 'echo "StrictHostKeyChecking=accept-new" >>~/.ssh/config'});
-  
+    await exec.exec("ssh -p 2222 root@localhost", [], { input: 'ssh-keygen -t rsa -f ~/.ssh/id_rsa -q -N ""' });
+    await exec.exec("ssh -p 2222 root@localhost", [], { input: 'echo "StrictHostKeyChecking=accept-new" >>~/.ssh/config' });
+
     let sshkey = "";
-    await exec.exec("ssh -p 2222 root@localhost", [], {input: 'cat ~/.ssh/id_rsa.pub', listeners:{stdout:(s)=>{
-      sshkey += s;
-    }}});
+    await exec.exec("ssh -p 2222 root@localhost", [], {
+      input: 'cat ~/.ssh/id_rsa.pub', listeners: {
+        stdout: (s) => {
+          sshkey += s;
+        }
+      }
+    });
 
     core.info("sshkey:" + sshkey);
 
     fs.writeFileSync(__dirname + "/id_rsa.pub", sshkey);
 
     core.info("Power off");
-    await exec.exec("ssh -p 2222 root@localhost", [], {input: 'shutdown -p now'});
+    await exec.exec("ssh -p 2222 root@localhost", [], { input: 'shutdown -p now' });
 
-    while(true) {
+    while (true) {
       core.info("Sleep 2 seconds");
       await sleep(2000);
-      let std="";
-      await exec.exec("sudo vboxmanage list runningvms", [], { listeners: {stdout:(s)=>{
-        std+=s;
-        core.info(s);
-      }}});
-      if(!std) {
+      let std = "";
+      await exec.exec("sudo vboxmanage list runningvms", [], {
+        listeners: {
+          stdout: (s) => {
+            std += s;
+            core.info(s);
+          }
+        }
+      });
+      if (!std) {
         core.info("shutdown OK continue.");
         await sleep(2000);
         break;
       }
     }
 
-    //await exec.exec("sudo vboxmanage controlvm "+ vmName + " poweroff");
-  
+    let ova = "freebsd-12.1.ova";
+    core.info("Export " + ova);
+    await vboxmanage(vmName, "export", "--output " + ova);
+    await exec.exec("sudo chmod 666 " + ova);
+
     core.info("Compress " + vhd);
-    await exec.exec("7z a freebsd-12.1.7z  id_rsa.pub "+ vhd);
+    await exec.exec("7z a freebsd-12.1.7z  id_rsa.pub " + ova);
 
   } catch (error) {
     core.setFailed(error.message);
