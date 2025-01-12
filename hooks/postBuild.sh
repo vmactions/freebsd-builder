ce04741da7a73fb24f32e686b69610e672a5397c
neil <gitpc@neilpang.com>
2025-01-12 11:48:59 +0100

    enable autologin with root

--- hooks/postBuild.sh
+++ hooks/postBuild.sh
@@ -18,5 +18,10 @@ service ntpd start
 
 
 
+#enable autologin with root in the console
+
+sed -E -i '' 's|ttyv0[[:space:]]+"/usr/libexec/getty Pc"|ttyv0	  "/usr/libexec/getty autologin"|' /etc/ttys
+
+
 
 
